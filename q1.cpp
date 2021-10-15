2a6590666d490137fc0e12792efe8f42b7ef4b9f
jrl0066 <jrl0066@auburn.edu>
2021-10-15 11:29:58 -0500

    Initial commit of q1
    initial commit using my own created code

--- q1.cpp
+++ q1.cpp
@@ -1,41 +1,68 @@
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
 
-class GasPump {
-	private:
-		double amountDispensed,
-		       amountCharged,
-		       costPerGallon;
+// double getAmount(int time, double cost);
+// double amountCharged(int time, double cost);
 
-	public:
-		double getAmountDispensed() { return amountDispensed; }
-		double getAmountCharged() { return amountCharged; }
-		double getCostPerGallon() { return costPerGallon; }
-		void setCostPerGallon(double cpg) { costPerGallon = cpg; }
-        
-		void dispense(int seconds) {
+//class for the Gas Pump
+class GasPump{
+           
+    public: 
+        double cost,
+               tGas = 0,
+               tCost = 0;
+        int time;
 
+        double getAmount(){
+            double gAmount = 0;
+            gAmount = 0.10 * time;
 
-			
-			amountCharged = amountDispensed * costPerGallon;
-		}
-		void reset() {
-            
-			amountDispensed = 0.0;
-			amountCharged = 0.0;
-		}		
+            return gAmount;
+        }
+        double amountCharged(){
+            double charged = 0;
+            charged = getAmount() * cost;
+
+            return charged;
+        }
+        double totalGas(){
+            tGas += getAmount();
+
+            return tGas;  
+        }    
+        double totalCharged(){
+            tCost += amountCharged();
+            return tCost;
+
+        }
 };
-	
+
 int main() {
-	
-    //Define variables
+    
+    GasPump pump;
+    char fillAgain = 'N';
 
-    // Define Class Object
+    cout << "Enter the cost of the gas per gallon: ";
+    cin >> pump.cost;
+    
+    do{
+        cout << "Enter how long you pump gas in seconds: ";
+        cin >> pump.time;
+        pump.totalGas();
+        pump.totalCharged();
+        cout << "\nAmount of gallons dispensed: " <<fixed << setprecision(2) << pump.getAmount() << endl;
+        cout << "Cost of gas per gallon: $" << fixed << setprecision(2) << pump.cost << endl;
+        cout << "Amount charged for gas: $" << fixed << setprecision(2) << pump.amountCharged() << endl;
 
-    // Call Functions
-	
+        cout << "\nEnter Y if you wish to dispense more gas: ";
+        cin >> fillAgain;
+    }
+    while(fillAgain == 'Y');
+    cout << "\nThe total gas dispensed is: " << pump.tGas << " gallons" << endl;
+    cout << "The total cost for gas dispemsed is: $" << pump.tCost;
 
-	return 0;
+    return 0;
 }
+
+
